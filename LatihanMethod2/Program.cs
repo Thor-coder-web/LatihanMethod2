81177dfba342163e746c4a255adebf8a0f1e1e71
Thor-coder-web <muhammad_hakim_34pg@student.smktelkom-mlg.sch.id>
2026-01-21 14:38:19 +0700

    Initial commit

---
+++ LatihanMethod2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Latihan Method 2  
+
+int harga = 20000;
+void hitungDiskon (int a)
+{
+
+    if (a == 5)
+    {
+        Console.WriteLine($"{a * harga}");
+    }
+    else if (a == 10)
+    {
+        Console.WriteLine($"{a * harga}");
+    }
+    else
+    {
+        Console.WriteLine($"Tidak mendapatkan potongan { a * harga}");
+    }
+
+}
+hitungDiskon (11);
